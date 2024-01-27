6388d7b3e35ece7c74d51b60b834b00f2be5728a
Oguzz <oguzaslan_203@hotmail.com>
2024-01-27 05:07:25 +0300

    Repository added

---
+++ src/main/java/com/example/flightsearchapi/repository/FlightRepository.java
@@ -0,0 +1,10 @@
+package com.example.flightsearchapi.repository;
+
+import com.example.flightsearchapi.entity.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FlightRepository extends JpaRepository<Flight, Long> {
+
+}
